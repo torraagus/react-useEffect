3f5ac2ef5f5b3f4102e2fde6900b83202bbeeb3b
broana-ait <torra.agustinignacio@gmail.com>
2020-05-27 14:04:29 -0300

    remove unused code. Added components FetchPosts and FetchPost.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import FetchPosts from './components/FetchPosts';
+import FetchPost from './components/FetchPost';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FetchPost id={1}/>
+      {/* <FetchPosts /> */}
     </div>
   );
 }
